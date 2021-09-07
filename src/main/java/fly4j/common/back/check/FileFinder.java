32d652f21ef71aed4c435a4a72f50448eb9225b5
qryc <qiuriyuchen@126.com>
2021-09-07 11:58:33 +0800

    优化代码

--- src/main/java/fly4j/common/back/check/FileFinder.java
+++ src/main/java/fly4j/common/back/check/FileFinder.java
@@ -2,17 +2,49 @@ package fly4j.common.back.check;
 
 import fly4j.common.lang.StringConst;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.TrueFileFilter;
 import org.apache.commons.lang3.StringUtils;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * 用于查找大文件
  * Created by qryc on 2016/5/4.
  */
-public class MaxFile {
+public class FileFinder {
 
+    public static void main(String[] args) {
+        String checkDirStr = "";
+        String findName = "target,classes";
+        var result = findFile(checkDirStr, findName);
+        System.out.println(result);
+    }
+
+    public static String findFile(String checkDirStr, String findName) {
+        StringBuilder info = new StringBuilder("文件查找结果：").append(StringUtils.LF);
+        Set<String> fileNameSet = new HashSet<>();
+        Arrays.stream(findName.split(",")).forEach(name -> fileNameSet.add(name));
+        info.append(" findName:").append(fileNameSet).append(StringUtils.LF);
+
+        info.append(checkDirStr).append(" check:").append(StringUtils.LF);
+
+        var files = FileUtils.listFilesAndDirs(new File(checkDirStr), TrueFileFilter.INSTANCE, TrueFileFilter.INSTANCE);
+        files.stream().filter(file -> {
+            for (String name : fileNameSet) {
+                if (file.getName().equals(name)) {
+                    return true;
+                }
+            }
+            return false;
+        }).forEach(file -> {
+            StringConst.appendLine(info, file.getAbsolutePath() + " " + FileUtils.byteCountToDisplaySize(file.length()));
+        });
+        return info.toString();
+    }
 
     public static String findMaxFileAndFloder(File dirFile, long alertFileSize, long alertFloderSize) {
         StringBuilder builder = new StringBuilder();
@@ -44,6 +76,7 @@ public class MaxFile {
         return builder.toString();
 
     }
+
     public static String maxFile(List<String> checkDirs, int bigFileSize) {
         StringBuilder info = new StringBuilder("检查大文件结果：").append(StringUtils.LF);
         checkDirs.forEach(checkDir -> {
