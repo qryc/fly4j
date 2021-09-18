6eb9c980934adfd9816525d19ecee530da3bbe6a
qryc <qiuriyuchen@126.com>
2021-09-18 19:04:19 +0800

    predicate

--- src/main/java/fly4j/common/lang/ExceptionUtil.java
+++ src/main/java/fly4j/common/lang/ExceptionUtil.java
@@ -1,5 +1,7 @@
 package fly4j.common.lang;
 
+import java.util.function.Function;
+
 /**
  * Created by qryc on 2021/9/10
  */
@@ -18,8 +20,8 @@ public class ExceptionUtil {
     }
 
     @FunctionalInterface
-    public static interface ExceptionWrapperR<T, E extends Exception> {
-        T doInTry() throws E;
+    public static interface ExceptionWrapperR<R, E extends Exception> {
+        R doInTry() throws E;
     }
 
     public static <T> T wrapperRuntimeR(ExceptionWrapperR<T, Exception> throwingConsumer) {
