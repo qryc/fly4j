ae77b993aea7c6453e23b19cc61a53286d1c711e
qryc <qiuriyuchen@126.com>
2022-02-07 17:17:37 +0800

    Mail非静态

---
+++ src/main/java/fly4j/common/mail/MailUtil2.java
@@ -0,0 +1,22 @@
+package fly4j.common.mail;
+
+import org.apache.commons.mail.EmailException;
+
+/**
+ * 非静态
+ * Created by qiuriyuchen on 2022/02/07.
+ */
+public class MailUtil2 {
+    private MailUtil.MailConfigModel mailConfigModel;
+
+    public void sendMail(String subject, String content, String filename, String mailTo) throws EmailException {
+        MailUtil.sendMail(mailConfigModel, subject, content, filename, mailTo);
+    }
+
+    public void sendSimpleMail(String subject, String content, String mailTo) throws EmailException {
+
+        MailUtil.sendSimpleMail(mailConfigModel, subject, content, mailTo);
+    }
+
+
+}
