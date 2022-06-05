0549f8e08399840db8fa2a1cabc37ba8079c25d6
qryc <qiuriyuchen@126.com>
2022-06-05 11:04:51 +0800

    ext Map

---
+++ src/main/java/fly4j/common/domain/IExtMap.java
@@ -0,0 +1,41 @@
+package fly4j.common.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import java.util.Map;
+
+public interface IExtMap<T> {
+    Map<String, String> getExtMap();
+
+    T setExtMap(Map<String, String> extMap);
+
+    @JsonIgnore
+    default Boolean getExtBooleanValue(String key) {
+        if (null == getExtMap().get(key)) {
+            return false;
+        }
+        return Boolean.valueOf(getExtMap().get(key));
+    }
+
+    @JsonIgnore
+    default String getExtStringValue(String key) {
+        return getExtMap().get(key);
+    }
+
+    @JsonIgnore
+    default String getExtStringValue(String key, String defaultValue) {
+        return getExtMap().get(key) == null ? defaultValue : getExtMap().get(key);
+    }
+
+    @JsonIgnore
+    default T setExtStringValue(String key, Object value) {
+        this.getExtMap().put(key, value.toString());
+        return (T) this;
+    }
+
+    @JsonIgnore
+    default T putExtAll(Map<String, String> paramMap) {
+        this.getExtMap().putAll(paramMap);
+        return (T) this;
+    }
+}
