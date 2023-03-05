7ddd9f49a8459258161c311961b591359d04912e
qryc <qiuriyuchen@126.com>
2023-03-05 17:49:20 +0800

    fileutil

--- src/main/java/fly4j/common/http/FileUpload.java
+++ src/main/java/fly4j/common/http/FileUpload.java
@@ -1,5 +1,6 @@
 package fly4j.common.http;
 
+import fly4j.common.util.DateUtil;
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
@@ -7,6 +8,7 @@ import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.nio.file.Path;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -77,6 +79,70 @@ public class FileUpload {
         return "";
     }
 
+    public static String uploadFileNewName(HttpServletRequest request, Path currPath, long sizeMaxM) throws Exception {
+        //如果不是文件上传，则返回
+        var isMultipart = ServletFileUpload.isMultipartContent(request);
+        if (!isMultipart) {
+            return "not file upload";
+        }
+
+
+        // 图片地址 ，相对于webroot的
+        var relativeWebRootPath = "";
+
+        // 允许上传的文件格式的列表
+        final var allpict = new String[]{".jpg", ".zip", ".jpeg", ".gif", ".jpeg", ".png", ".bmp", ".JPG", ".JPGE", ".GIF", ".JPEG", ".PNG",
+                ".BMP"};
+
+        var factory = new DiskFileItemFactory();
+        // 设置临时目录：
+        var tempPath = request.getSession().getServletContext().getRealPath("/temp");
+        factory.setRepository(new File(tempPath));
+        // 设置缓冲区大小，这里是40M
+        factory.setSizeThreshold(41943040);
+
+        ServletFileUpload upload = new ServletFileUpload(factory);
+        // 设置最大文件4MB
+        upload.setSizeMax(sizeMaxM * 1024 * 1024);
+        // 得到所有的文件：
+        List<FileItem> fileList = upload.parseRequest(request);
+
+
+        for (var fileItem : fileList) {
+            if (fileItem.isFormField()) {//如果是表单域，就不是文件上传
+                continue;
+            }
+
+            String fileName = fileItem.getName();
+            long size = fileItem.getSize();
+            if ((fileName == null || fileName.equals("")) && size == 0) {
+                continue;
+            }
+
+            var fileTypeName = getFileType(allpict, fileName);
+            // 图片路径
+            var picPath = "iknowData" + fileTypeName;
+
+            // 返回文件名，以时间为名，路径：e:/temp
+            var savePath = Path.of(currPath.toString(), fileName);
+            if ("clipBoardDefault.png".equals(fileName)) {
+                savePath = Path.of(currPath.toString(), DateUtil.getDateStr4Name(new Date()) + ".png");
+            }
+            var file = savePath.toFile();
+            //返回相对路径
+            relativeWebRootPath = "pic/" + picPath;
+//            if (!file.getParentFile().isDirectory()) {
+//                file.getParentFile().mkdirs();// 创建目录
+//            }
+
+
+            fileItem.write(file);
+            return file.getAbsolutePath();
+
+
+        }
+        return "";
+    }
 
     private static String getFileType(final String[] allpict, String fileName) {
         String fileTypeName = fileName.substring(fileName.lastIndexOf("."), fileName.length());
