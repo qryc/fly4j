aed891610aa33075e9cecee7d9ca3a6d173f6b71
qryc <qiuriyuchen@126.com>
2023-05-13 15:02:05 +0800

    map util

--- src/main/java/fly4j/common/util/map/LinkedHashMapUtil.java
+++ src/main/java/fly4j/common/util/map/LinkedHashMapUtil.java
@@ -1,12 +1,38 @@
 package fly4j.common.util.map;
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 /**
  * Created by qryc on 2021/9/9
  */
 public class LinkedHashMapUtil {
+    public static <K, V> LinkedHashMap<K, V> filterLinkedHashMap(LinkedHashMap<K, V> linkedHashMap, Predicate<Map.Entry<K, V>> predicate) {
+        LinkedHashMap resultMap = new LinkedHashMap<>();
+        for (Map.Entry entry : linkedHashMap.entrySet()) {
+            if (predicate.test(entry)) {
+                resultMap.put(entry.getKey(), entry.getValue());
+            }
+        }
+        return resultMap;
+    }
+
+    public static <K, V> LinkedHashMap<V, List<K>> convert2ValueMap(Map<K, V> map) {
+        LinkedHashMap<V, List<K>> valueMap = new LinkedHashMap<>();
+        map.forEach((k, v) -> {
+            List<K> keys = valueMap.computeIfAbsent(v, key -> new ArrayList<>());
+            keys.add(k);
+        });
+        return valueMap;
+    }
+
+    /**
+     * 变更有序Map的Key
+     */
     public static <K, V, R> LinkedHashMap<R, V> alterKey(LinkedHashMap<K, V> map, Function<K, R> function) {
         LinkedHashMap<R, V> newMap = new LinkedHashMap<>();
         map.forEach((k, v) -> {
--- src/main/java/fly4j/common/util/map/MapUtil.java
+++ src/main/java/fly4j/common/util/map/MapUtil.java
@@ -4,24 +4,7 @@ import java.util.*;
 import java.util.function.Predicate;
 
 public class MapUtil {
-    public static <K, V> LinkedHashMap<K, V> filterLinkedHashMap(LinkedHashMap<K, V> linkedHashMap, Predicate<Map.Entry<K, V>> predicate) {
-        LinkedHashMap resultMap = new LinkedHashMap<>();
-        for (Map.Entry entry : linkedHashMap.entrySet()) {
-            if (predicate.test(entry)) {
-                resultMap.put(entry.getKey(), entry.getValue());
-            }
-        }
-        return resultMap;
-    }
 
-    public static <K, V> LinkedHashMap<V, List<K>> convert2ValueMap(Map<K, V> map) {
-        LinkedHashMap<V, List<K>> valueMap = new LinkedHashMap<>();
-        map.forEach((k, v) -> {
-            List<K> keys = valueMap.computeIfAbsent(v, key -> new ArrayList<>());
-            keys.add(k);
-        });
-        return valueMap;
-    }
 
     public static Boolean getBooleanDFalse(Map<String, String> map, String key) {
         if (null == map.get(key)) {
