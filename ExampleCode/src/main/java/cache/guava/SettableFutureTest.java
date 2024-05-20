3417091de38f3c48da3d8bdfa0d154b4c8cd2edc
qryc <qiuriyuchen@126.com>
2024-05-20 22:16:18 +0800

    futuretest

--- ExampleCode/src/main/java/cache/guava/SettableFutureTest.java
+++ ExampleCode/src/main/java/cache/guava/SettableFutureTest.java
@@ -7,8 +7,8 @@ import java.util.concurrent.TimeUnit;
 public class SettableFutureTest {
     public static void main(String[] args) throws Exception {
         SettableFuture<String> settableFuture = SettableFuture.create();
-//        settableFuture.set("ok");
-        settableFuture.setException(new RuntimeException("fail"));
+        settableFuture.set("ok");
+//        settableFuture.setException(new RuntimeException("fail"));
         System.out.println(settableFuture.get(10, TimeUnit.MILLISECONDS));
     }
 }
