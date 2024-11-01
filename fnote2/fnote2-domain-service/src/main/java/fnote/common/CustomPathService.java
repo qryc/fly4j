639eb3ee57832b814f1c628972a3924b63aabbf5
qryc <qiuriyuchen@126.com>
2024-11-01 11:00:03 +0800

    add custom article path config feature

--- fnote2/fnote2-domain-service/src/main/java/fnote/common/CustomPathService.java
+++ fnote2/fnote2-domain-service/src/main/java/fnote/common/CustomPathService.java
@@ -11,11 +11,13 @@ import java.util.stream.Stream;
 
 public class CustomPathService extends StorePathService {
     protected Map<String, String> user2CustomRoot = new HashMap<>();
-
+    private String customPath;
 
     public void init() {
-
-       //todo 解析配置文件
+        String uname = customPath.split(",")[0];
+        String path = customPath.split(",")[1];
+        user2CustomRoot.put(uname, path);
+        //todo 解析配置文件
 
 
     }
@@ -23,14 +25,13 @@ public class CustomPathService extends StorePathService {
 
     @Override
     public List<Path> getAllArticleDirPaths(String pin) {
-        List<String> customPaths = this.getUserCustomPaths(pin, customDocPath);
-        if (CollectionUtils.isEmpty(customPaths)) {
+        if (StringUtils.isEmpty(user2CustomRoot.get(pin))) {
             //默认路径
             return super.getAllArticleDirPaths(pin);
         } else {
             //如果有自定义路径，优先返回自定义路径
-            return customPaths.stream()
-                    .map(str -> Path.of(str)).toList();
+            Path path = Path.of(user2CustomRoot.get(pin));
+            return List.of(path);
 
         }
     }
@@ -38,8 +39,8 @@ public class CustomPathService extends StorePathService {
 
     @Override
     public List<Path> getFlyDiskCanSelect(FlyContext flyContext) {
-        return this.getUserCustomPaths(flyContext.getPin(), customDiskPath).stream().map(str -> Path.of(str))
-                .toList();
+        Path path = Path.of(user2CustomRoot.get(flyContext.getPin()));
+        return List.of(path);
     }
 
     /**
@@ -47,40 +48,21 @@ public class CustomPathService extends StorePathService {
      */
     @Override
     public List<Path> getUserPicDirPaths(String pin) {
-        //用户默认图片路径
-        if (StringUtils.isBlank(customPicBedPath.get(pin))) {
-            return super.getUserPicDirPaths(pin);
-        } else {
-            //自定义图床
-            return Stream.of(Path.of(PropertisUtil.adjustPath(customPicBedPath.get(pin)))).toList();
-        }
+        Path path = Path.of(user2CustomRoot.get(pin));
+        return List.of(path);
 
 
-    }
-
-    public List<String> getUserCustomPaths(String pin, Map<String, String> pathMap) {
-        if (StringUtils.isBlank(pathMap.get(pin))) {
-            return new ArrayList<>();
-        }
-        return Arrays.stream(pathMap.get(pin).split(",")).map(str -> PropertisUtil.adjustPath(str)).toList();
     }
 
     public List<Path> getUserDiskDirPaths(FlyContext flyContext) {
 
-
-        //根据用户的自定义路径，叠加过滤
-        List<String> customPaths = this.getUserCustomPaths(flyContext.getPin(), customDiskPath);
-        if (CollectionUtils.isEmpty(customPaths)) {
-            //用户未配置，返回用户默认
-            return super.getUserDiskDirPaths(flyContext);
-        } else {
-            //返回用户自定义配置
-            return customPaths.stream()
-                    .filter(pathStr -> flyContext.clientConfig().getShowFolderList().contains(pathStr))
-                    .map(str -> Path.of(str))
-                    .collect(Collectors.toList());
-        }
+        Path path = Path.of(user2CustomRoot.get(flyContext.getPin()));
+        return List.of(path);
 
 
+    }
+
+    public void setCustomPath(String customPath) {
+        this.customPath = customPath;
     }
 }
