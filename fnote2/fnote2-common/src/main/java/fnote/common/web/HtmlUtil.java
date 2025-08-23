3c88cc163a1969f6520420dceeead7fd25dee658
qryc <qiuriyuchen@126.com>
2025-08-23 10:41:31 +0800

    fix pic bed filter

---
+++ fnote2/fnote2-common/src/main/java/fnote/common/web/HtmlUtil.java
@@ -0,0 +1,27 @@
+package fnote.common.web;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class HtmlUtil {
+    private static String srcInImgHtmlRegex = "<img[^>]+src\\s*=\\s*['\"]([^'\"]+)['\"][^>]*>";
+
+    public static List<String> getSrcFromImagTag(String html) {
+        List<String> srcList = new ArrayList<>();
+        Pattern imgPattern = Pattern.compile(srcInImgHtmlRegex);
+        Matcher matcher = imgPattern.matcher(html);
+        while (matcher.find()) {
+            srcList.add(matcher.group(1));
+        }
+        return srcList;
+    }
+
+    public static void main(String[] args) {
+        String html = "<img src='example1.jpg'> <img src=\"example2.jpg\"> <img src=\"example3.jpg\">";
+        List<String> srcList = getSrcFromImagTag(html);
+        srcList.forEach(System.out::println);
+    }
+}
--- fnote2/fnote2-domain-service/src/main/java/farticle/domain/extension/impl/query/view/PicBedFilter.java
+++ fnote2/fnote2-domain-service/src/main/java/farticle/domain/extension/impl/query/view/PicBedFilter.java
@@ -3,6 +3,7 @@ package farticle.domain.extension.impl.query.view;
 import farticle.domain.extension.query.ArticleViewFilter;
 import farticle.domain.view.ArticleView;
 import fnote.common.PathService;
+import fnote.common.web.HtmlUtil;
 import fnote.domain.config.FlyConfig;
 import fnote.domain.config.FlyContext;
 
@@ -12,6 +13,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -20,7 +22,7 @@ public class PicBedFilter implements ArticleViewFilter {
     private PathService pathService;
     private Map<String, String> orignToReplese4Local = new HashMap<>();
     private boolean replace2Local = false;
-    private String srcInImgHtmlRegex="<img[^>]+src\\s*=\\s*['\"]([^'\"]+)['\"][^>]*>";
+
 
     public PicBedFilter() {
         orignToReplese4Local.put("http://fly4j.cn/", "http://localhost:8080/");
@@ -38,33 +40,19 @@ public class PicBedFilter implements ArticleViewFilter {
         }
 
         //replace absolute img path
-        Pattern imgPattern = Pattern.compile(srcInImgHtmlRegex);
-        Matcher matcher = imgPattern.matcher(articleView.getHtml());
-        while (matcher.find()) {
-            //match find src
-            String src = matcher.group(1);
+        List<String> srcList = HtmlUtil.getSrcFromImagTag(articleView.getHtml());
+        for (String src : srcList) {
             //absolute src path
             Path srcPath = Path.of(articleView.getCplArticle().getNoteFileStr()).getParent().resolve(URLDecoder.decode(src, StandardCharsets.UTF_8));
             if (Files.exists(srcPath)) {
                 //replace src with absolute src
-                content = content.replaceAll(src, URLEncoder.encode(srcPath.toString(),StandardCharsets.UTF_8));
+                content = content.replaceAll(src, URLEncoder.encode(srcPath.toString(), StandardCharsets.UTF_8));
             }
         }
         articleView.setHtml(content);
     }
 
-    public static void main(String[] args) {
-        String html = "<img src='example1.jpg'> <img src=\"example2.jpg\"> <img src=\"example3.jpg\">";
-        String pattern = "<img[^>]+src\\s*=\\s*['\"]([^'\"]+)['\"][^>]*>";
 
-        Pattern imgPattern = Pattern.compile(pattern);
-        Matcher matcher = imgPattern.matcher(html);
-
-        while (matcher.find()) {
-            String src = matcher.group(1);
-            System.out.println("src: " + src);
-        }
-    }
 
 
     public void setPathService(PathService pathService) {
